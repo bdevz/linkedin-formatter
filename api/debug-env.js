f91f9a1dd38c26415116e98ddf5aef8ce937e638
bdevz <87504907+bdevz@users.noreply.github.com>
2026-04-27 13:12:21 -0400

    Remove temporary debug endpoint

--- api/debug-env.js
+++
@@ -1,9 +0,0 @@
-export default function handler(req, res) {
-  return res.status(200).json({
-    hasAnthropicKey: !!process.env.ANTHROPIC_API_KEY,
-    hasOpenAIKey: !!process.env.OPENAI_API_KEY,
-    anthropicKeyPrefix: process.env.ANTHROPIC_API_KEY ? process.env.ANTHROPIC_API_KEY.slice(0, 6) + '...' : null,
-    openaiKeyPrefix: process.env.OPENAI_API_KEY ? process.env.OPENAI_API_KEY.slice(0, 6) + '...' : null,
-    nodeVersion: process.version,
-  });
-}
